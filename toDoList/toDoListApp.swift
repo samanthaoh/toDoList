38ac68ec15e986d2ee6f949dee807d7ee75c4ca0
samanthaoh <samanthagrace1818@gmail.com>
2023-06-28 10:48:17 -0400

    Initial Commit

---
+++ toDoList/toDoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  toDoListApp.swift
+//  toDoList
+//
+//  Created by Samantha Oh on 6/28/23.
+//
+
+import SwiftUI
+
+@main
+struct toDoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
